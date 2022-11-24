cf278508c1dcbb9dccda9da91b404b062a9c21fc
Rui Junior <77105367+rui-junior@users.noreply.github.com>
2022-11-24 07:56:38 -0300

    Add files via upload

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+
+import style from '../styles/index.module.css'
+import Form from '../components/Form'
+
+function Index(){
+    
+
+    return(
+
+        <div className={style.container}>
+
+            <Form />
+
+        </div>
+
+    )
+
+}
+
+export default Index
